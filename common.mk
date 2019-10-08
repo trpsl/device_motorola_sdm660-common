abcbe35f2c512edd1d95b9e6314dbb857dde2c70
LuK1337 <priv.luk@gmail.com>
2019-10-08 20:12:52 +0200

    sdm660-common: Update wfd stack from mata
    Change-Id: I4f032b7cf87bd2b9010aa2158a7678da490e2aed

--- common.mk
+++ common.mk
@@ -452,3 +452,10 @@ PRODUCT_PACKAGES += \
     wpa_supplicant \
     wpa_supplicant.conf \
     wpa_supplicant_wcn.conf
+
+# Wi-Fi Display
+PRODUCT_PACKAGES += \
+    libmediaextractorservice
+
+PRODUCT_BOOT_JARS += \
+    WfdCommon
