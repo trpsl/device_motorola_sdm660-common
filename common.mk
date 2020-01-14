1223346f5e27b56c4f2ebf7a4459b65fc62a806a
Rohan Hasabe <43720061+Hasaber8@users.noreply.github.com>
2020-01-14 15:10:21 +0530

    sdm660-common: Use 4GB dalvik-heap config

--- common.mk
+++ common.mk
@@ -100,15 +100,6 @@ PRODUCT_PACKAGES += \
     com.android.media.swcodec \
     libsfplugin_ccodec
 
-# Dalvik
-PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.heapgrowthlimit=256m \
-    dalvik.vm.heapstartsize=8m \
-    dalvik.vm.heapsize=512m \
-    dalvik.vm.heaptargetutilization=0.75 \
-    dalvik.vm.heapminfree=512k \
-    dalvik.vm.heapmaxfree=8m
-
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
