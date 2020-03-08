14214d2348f964ec0a38eed4dfdc0a65d1556d42
trpsl <trpsl755@gmail.com>
2020-03-08 23:35:15 +0000

    sdm660-common: Cleanup for Pixel Experience

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -119,7 +119,6 @@ TARGET_USES_QTI_CAMERA_DEVICE := true
 
 # Charger
 BOARD_CHARGER_ENABLE_SUSPEND := true
-WITH_LINEAGE_CHARGER := false
 
 # Display
 MAX_EGL_CACHE_KEY_SIZE := 12*1024
@@ -187,9 +186,6 @@ TARGET_KERNEL_SOURCE := kernel/motorola/msm8998
 
 # Lights
 TARGET_PROVIDES_LIBLIGHT := true
-
-# Lineage hardware
-JAVA_SOURCE_OVERLAYS := org.lineageos.hardware|$(PLATFORM_PATH)/lineagehw|**/*.java
 
 # Partitions
 BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
